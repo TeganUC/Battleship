efbe4ce09d2e011da2a347a59d1b9beaacb7228e
TeganUC <rohnetd@mail.uc.edu>
2025-03-27 13:17:11 -0400

    h

--- src/BattleshipFrame.java
+++ src/BattleshipFrame.java
@@ -8,11 +8,18 @@ public class BattleshipFrame extends JFrame implements ActionListener{
 
     private JButton[][] buttons = new JButton[10][10];
     private String[][] board = new String[10][10];
-    private JPanel mainPnl, cmdPnl, btnPnl;
+    private JPanel mainPnl, cmdPnl, countPnl, btnPnl;
     private JButton newBtn, quitBtn;
+    private JLabel hits, misses, strikes, totalMisses;
     private boolean gameWon = false;
     private boolean gameEnded = false;
     private Random random = new Random();
+
+    int numHits = 0;
+    int numMisses = 0;
+    int numStrikes = 0;
+    int numTotalMisses = 0;
+    int flukes = 0;
 
     public BattleshipFrame() {
         setTitle("Battleship");
@@ -38,7 +45,7 @@ public class BattleshipFrame extends JFrame implements ActionListener{
             }
         }
 
-        add(btnPnl);
+        mainPnl.add(btnPnl);
 
         Boat boat1 = new Boat(board, 5);
         boat1.placeShip(board, 5);
@@ -55,6 +62,7 @@ public class BattleshipFrame extends JFrame implements ActionListener{
         Boat boat5 = new Boat(board, 2);
         boat5.placeShip(board, 2);
 
+        createCounters();
         createControlPanel();
 
         setVisible(true);
@@ -68,31 +76,78 @@ public class BattleshipFrame extends JFrame implements ActionListener{
             for (int j = 0; j < buttons[i].length; j++) {
 
                 if(buttons[i][j] == buttonClicked){
-                    if(buttons[i][j] == buttonClicked && board[i][j] == "S"){
+                    if(buttons[i][j] == buttonClicked && board[i][j] == "S") {
 
                         buttonClicked.setForeground(new Color(255, 41, 84));
                         buttonClicked.setText("X");
 
-                        //add hit to the counter
+                        board[i][j] = "H";
+
+                        numHits++;
+                        hits.setText("Hits: " + numHits);
+
+                        numMisses = 0;
+                        misses.setText("Misses: " + numMisses);
+
+                    }else if(((buttons[i][j] == buttonClicked && board[i][j] == "H") || (buttons[i][j] == buttonClicked && board[i][j] == "M")) && (board[i][j] != "W") && ((board[i][j] != "S"))) {
+
+                        flukes++;
+
                     }else{
 
                         buttonClicked.setForeground(new Color(252, 232, 96));
                         buttonClicked.setText("M");
+                        board[i][j] = "M";
 
-                        //add to counter
-                        //second if statement for 5 misses
+                        numMisses++;
+                        misses.setText("Misses: " + numMisses);
 
+                        numTotalMisses++;
+                        totalMisses.setText("Miss Total: " + numTotalMisses);
+
+
+                        if(numMisses == 5){
+                            numStrikes++;
+                            strikes.setText("Strikes: " + numStrikes);
+
+                            numMisses = 0;
+                            misses.setText("Misses: " + numMisses);
+                        }
+
+                        if(numStrikes == 3){
+                            restartGame();
+                            int popUp = JOptionPane.showConfirmDialog(null, "You lose. Game restarted!", "Confirmation", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+                        }
                     }
                 }
-
-
+                if(checkForWin()){
+                    restartGame();
+                    int popUp = JOptionPane.showConfirmDialog(null, "You win! Game restarted!", "Confirmation", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+                }
             }
         }
-
-
     }
 
-    //add buttons & counters here
+    private void createCounters(){
+
+        countPnl = new JPanel();
+        countPnl.setLayout(new GridLayout(2,2));
+
+        hits = new JLabel("Hits: " + numHits);
+        countPnl.add(hits);
+
+        misses = new JLabel("Misses: " + numMisses);
+        countPnl.add(misses);
+
+        strikes = new JLabel("Strikes: " + numStrikes);
+        countPnl.add(strikes);
+
+        totalMisses = new JLabel("Miss Total: " + numTotalMisses);
+        countPnl.add(totalMisses);
+
+        mainPnl.add(countPnl);
+    }
+
     private void createControlPanel() {
         Random rnd = new Random();
         cmdPnl = new JPanel();
@@ -101,8 +156,12 @@ public class BattleshipFrame extends JFrame implements ActionListener{
 
         newBtn.addActionListener((ActionEvent ae) -> {
 
-            // code to reset everything
+            int choice = JOptionPane.showConfirmDialog(null, "Are you sure?", "Confirmation", JOptionPane.YES_NO_OPTION);
 
+            if (choice == JOptionPane.YES_OPTION) {
+                restartGame();
+                JOptionPane.showMessageDialog(null, "Game restarted!");
+            }
         });
 
         quitBtn = new JButton("Quit!");
@@ -112,10 +171,73 @@ public class BattleshipFrame extends JFrame implements ActionListener{
         cmdPnl.add(quitBtn);
 
         mainPnl.add(cmdPnl);
-
     }
 
+    private void restartGame(){
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+                board[i][j] = null;
+
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+                board[i][j] = "W";
+
+            }
+        }
 
 
 
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons[i].length; j++) {
+
+                buttons[i][j].setText("W");
+                buttons[i][j].setForeground(new Color(25, 163, 255));
+                buttons[i][j].addActionListener(this);
+                btnPnl.add(buttons[i][j]);
+            }
+        }
+
+        Boat boat1 = new Boat(board, 5);
+        boat1.placeShip(board, 5);
+
+        Boat boat2 = new Boat(board, 4);
+        boat2.placeShip(board, 4);
+
+        Boat boat3 = new Boat(board, 3);
+        boat3.placeShip(board, 3);
+
+        Boat boat4 = new Boat(board, 3);
+        boat4.placeShip(board, 3);
+
+        Boat boat5 = new Boat(board, 2);
+        boat5.placeShip(board, 2);
+
+        numHits = 0;
+        numMisses = 0;
+        numTotalMisses = 0;
+        numStrikes = 0;
+
+        hits.setText("Hits: " + numHits);
+        misses.setText("Misses: " + numStrikes);
+        totalMisses.setText("Miss Total: " + numStrikes);
+        strikes.setText("Strikes: " + numStrikes);
+    }
+
+    private boolean checkForWin(){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+                if(board[i][j] == "S"){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
